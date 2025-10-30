532582e669ac17cbf58487a9ce158716f7e0fece
nabim777 <nabinalemagar019@gmail.com>
2025-10-30 21:25:32 +0545

    add trace on config file
    Signed-off-by: nabim777 <nabinalemagar019@gmail.com>

--- cucumber.conf.js
+++ cucumber.conf.js
@@ -21,11 +21,15 @@ AfterAll(async function () {
 // Create a new browser context and page per scenario
 Before(async function () {
   global.context = await global.browser.newContext();
+  await global.context.tracing.start({ screenshots: true, snapshots: true });
   global.page = await global.context.newPage();
 });
 
 // Cleanup after each scenario
-After(async function () {
+After(async function (scenario) {
+  if (scenario.result.status === 'FAILED') {
+    await global.context.tracing.stop({ path: `trace-${Date.now()}.zip` });
+  }
   await global.page.close();
   await global.context.close();
 });
